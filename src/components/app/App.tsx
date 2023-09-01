037495a735f07901f3e19d6c4da8ab3288e8b841
Martin Alemajoh <alemajohmartin@gmail.com>
2023-08-31 23:19:07 -0400

    signup page. create and designed signup page

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,5 @@
 import Login from "../../pages/login/login";
+// import SignUp from "../../pages/signup/Signup";
 
 function App() {
   return (
--- src/pages/signup/Signup.tsx
+++ src/pages/signup/Signup.tsx
@@ -1,17 +1,51 @@
 import Logo from "./../../assets/Fahoot Logo.svg";
 
-export default function Login() {
+export default function SignUp() {
   return (
     <>
       <div className="flex min-h-full flex-1 flex-col justify-center py-12 sm:px-6 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-md">
           <img className="mx-auto h-20 w-auto" src={Logo} alt="Fahoot" />
-          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-secondary-500">Sign in to your account</h2>
+          <h2 className="mt-6 text-center text-2xl font-bold leading-9 tracking-tight text-secondary-500">Sign up for a new account</h2>
         </div>
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
           <div className="bg-white px-6 py-12 shadow sm:rounded-lg sm:px-12">
             <form className="space-y-6" action="#" method="POST">
+              <div className="flex items-center justify-between gap-x-5">
+                <div>
+                  <label htmlFor="first_name" className="block text-sm font-medium leading-6 text-secondary-900">
+                    First name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="first_name"
+                      name="first_name"
+                      type="text"
+                      autoComplete="off"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-secondary-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+
+                <div>
+                  <label htmlFor="last_name" className="block text-sm font-medium leading-6 text-secondary-900">
+                    Last name
+                  </label>
+                  <div className="mt-2">
+                    <input
+                      id="last_name"
+                      name="last_name"
+                      type="text"
+                      autoComplete="off"
+                      required
+                      className="block w-full rounded-md border-0 py-1.5 text-secondary-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-primary-600 sm:text-sm sm:leading-6"
+                    />
+                  </div>
+                </div>
+              </div>
+
               <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-secondary-900">
                   Email address
@@ -44,26 +78,11 @@ export default function Login() {
                 </div>
               </div>
 
-              <div className="flex items-center justify-between">
-                <div className="flex items-center">
-                  <input id="remember-me" name="remember-me" type="checkbox" className="h-4 w-4 rounded border-gray-300 text-primary-600 focus:ring-primary-600" />
-                  <label htmlFor="remember-me" className="ml-3 block text-sm leading-6 text-gray-900">
-                    Remember me
-                  </label>
-                </div>
-
-                <div className="text-sm leading-6">
-                  <a href="#" className="font-semibold text-secondary-600 hover:text-secondary-500">
-                    Forgot password?
-                  </a>
-                </div>
-              </div>
-
               <div>
                 <button
                   type="submit"
                   className="flex w-full justify-center rounded-md bg-primary-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-primary-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-600 transition-all duration-300 ease-linear">
-                  Sign in
+                  Sign up
                 </button>
               </div>
             </form>
@@ -96,9 +115,9 @@ export default function Login() {
           </div>
 
           <p className="mt-10 text-center text-sm text-gray-500">
-            Not a member?{" "}
+            Already a member?{" "}
             <a href="#" className="font-semibold leading-6 text-primary-600 hover:text-primary-500">
-              Sign up here
+              Sign in here
             </a>
           </p>
         </div>
